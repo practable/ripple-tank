125a719902cabb1352c8a296bffeb2701a773ba1
david <dprydereid@gmail.com>
2026-02-20 15:18:45 +0000

    Workspace (fix): changed justification of ruler control tools so that on small screens the overflow scroll shows all the buttons

--- ui/default/src/components/Workspace.vue
+++ ui/default/src/components/Workspace.vue
@@ -21,7 +21,7 @@
 
     <!-- COMPONENT FOR MANIPULATING SIZE AND ANGLE OF RULER -->
     <div class="col-lg-10 ms-2" style="overflow-x: scroll;">
-        <div class="d-flex flex-row align-items-center justify-content-center">
+        <div class="d-flex flex-row align-items-center ">
             <button id='rotate-ruler-anti-clockwise-large-button' class='button-sm button-primary me-2' @mousedown='rotateTool(-10, "ruler")'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.5 2v6h6M2.66 15.57a10 10 0 1 0 .57-8.38"/></svg>
             </button>
@@ -61,7 +61,7 @@
             </button>
         </div>
 
-        <div class="d-flex flex-row align-items-center justify-content-center">
+        <div class="d-flex flex-row align-items-center">
             <button class='button-sm button-primary me-2' @mousedown='rotateTool(-10, "protractor")'>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M2.5 2v6h6M2.66 15.57a10 10 0 1 0 .57-8.38"/></svg>
             </button>
